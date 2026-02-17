8c01792437484757089fa7cd4dee186382ec2118
ms4313 <ms4313@srmist.edu.in>
2026-02-17 09:30:40 +0530

    coded UC2 to Render OOPS as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,17 @@
 /**
-* OOPSBannerApp UC1 - OOPS Banner Display Application
-* @author Developer
-* @version 1.0
-*/
-public class OOPSBannerApp{
-	public static void main(String[] args){
-		System.out.println("OOPS");
-	}
+ * OOPSBannerApp UC2 – Banner Display Application
+ * @author Developer
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("  *****  " + "  *****  " + " ******  " + " ****** ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *      ");
+        System.out.println("*       *" + "*       *" + " *     * " + " *      ");
+        System.out.println("*       *" + "*       *" + " ******  " + " ****** ");
+        System.out.println("*       *" + "*       *" + " *       " + "      * ");
+        System.out.println(" *     * " + " *     * " + " *       " + " *    * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  ****  ");
+    }
 }
